bd30182feb6a032020469286a84d881a89727370
Maurício Caminha <mauricio.caminha@hotmail.com>
2023-09-14 13:36:09 -0300

    [feat] add response stream openAI

--- src/routes/generate-ai-completion.ts
+++ src/routes/generate-ai-completion.ts
@@ -1,6 +1,6 @@
 import { FastifyInstance } from "fastify";
-import { createReadStream } from 'node:fs';
 import { z } from 'zod';
+import { streamToResponse, OpenAIStream } from 'ai';
 import { prisma } from "../lib/prisma";
 import { openai } from "../lib/openai";
 
@@ -31,10 +31,19 @@ export async function generationAICompletionRoute(app: FastifyInstance) {
       temperature,
       messages: [
         { role: 'user', content: promptMessage }
-      ]
+      ],
+      stream: true,
+
     })
 
-    return response;
+    const stream = OpenAIStream(response);
+
+    streamToResponse(stream, reply.raw, {
+      headers: {
+        'Acces-Control-Allow-Origin': '*',
+        'Acces-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS'
+      }
+    })
   });
 
 }
